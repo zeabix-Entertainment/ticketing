f807f30d8a0250e8efc73ae3213e9f19972234b8
kamonwat S <kamonwat.s@zeabix.com>
2022-04-26 15:17:38 +0700

    sdfdsfdsf

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://z-commerce-dev.zeabix.com/',
+      baseURL: process.env.baseURL,
       headers: req.headers,
     });
   } else {
