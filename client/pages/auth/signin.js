3f2ac5e822d96745ee74a14951eb744e61fe3846
kamonwat S <kamonwat.s@zeabix.com>
2022-04-26 17:41:52 +0700

    sddddd

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -42,7 +42,7 @@ export default () => {
         />
       </div>
       {errors}
-      <button className="btn btn-primary">Sign In-test-tag1234321</button>
+      <button className="btn btn-primary">Sign In-test-tag1234321-dfsfsdfdfs</button>
     </form>
   );
 };
