69369532f46043a2bb28a4b26f36aec6e555b468
kaicode123 <kai_ja002@hotmail.com>
2022-05-09 16:39:55 +0700

    sdfsdsdf

--- client/next.config.js
+++ client/next.config.js
@@ -1,28 +1,7 @@
-const webpack = require("webpack");
 
-module.exports = {
-  webpack: (config) => {
-    config.node = {
-      fs: "empty",
-    };
-
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-    const env = Object.keys(process.env).reduce((acc, curr) => {
-      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-      return acc;
-    }, {});
-
-    config.plugins.push(new webpack.DefinePlugin(env));
-
-    return config;
-  },
-}; 
-
-/*
 module.exports = {
   webpackDevMiddleware: (config) => {
     config.watchOptions.poll = 300;
     return config;
   },
 };
-*/
